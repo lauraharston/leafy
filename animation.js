3c87a632cb456d5b25bb6fef676f3268ffe3a2f9
lauraharston <lauraharston@gmail.com>
2020-11-17 12:45:08 -0600

    added a match media function for the founder2 img

--- animation.js
+++ animation.js
@@ -1,3 +1,19 @@
+// Window match media
+let picture = document.getElementById("founder2");
+let parent = document.getElementById("martin");
+
+function moveImg () {
+    if (window.matchMedia("(max-width: 570px)").matches) {
+        parent.prepend(picture);
+      } else {
+          parent.appendChild(picture);
+      }
+}
+
+moveImg();
+
+
+// Animations
 let tl = gsap.timeline();
 
 tl.from("#hand", {duration: 1, opacity: 0, x: 150, stagger: 0.3})
