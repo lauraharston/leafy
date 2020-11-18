c0e212903b2ef5841aabf34035e8637511f06fd3
lauraharston <lauraharston@gmail.com>
2020-11-18 16:42:26 -0600

    d-none class to paragraphs to the intro section

--- animation.js
+++ animation.js
@@ -1,7 +1,10 @@
 // Window match media
 let picture = document.getElementById("founder2");
 let parent = document.getElementById("martin");
-let mediaResize = window.matchMedia("(max-width: 812px)")
+let mediaResize = window.matchMedia("(max-width: 812px)");
+let introduction= document.getElementById("introduction");
+let pinkPicture = document.getElementById("pink");
+let mediaIntro = window.matchMedia("(max-width: 415px)");
 
 function moveImg () {
     if (mediaResize.matches) {
@@ -14,8 +17,14 @@ function moveImg () {
 moveImg(mediaResize);
 mediaResize.addListener(moveImg);
 
+function moveTextBlock () {
+    if(mediaIntro.matches) {
+        pinkPicture.remove();
+    } 
+}
 
-
+moveTextBlock (mediaIntro); 
+mediaIntro.addListener(moveTextBlock);
 // Animations
 let tl = gsap.timeline();
 
