6de56b6f913b4fcc0751a6784ae3ffed0bb30d8a
lauraharston <lauraharston@gmail.com>
2020-11-23 16:10:24 -0600

    added navbar

--- animation.js
+++ animation.js
@@ -2,9 +2,7 @@
 let picture = document.getElementById("founder2");
 let parent = document.getElementById("martin");
 let mediaResize = window.matchMedia("(max-width: 812px)");
-let introduction= document.getElementById("introduction");
-let pinkPicture = document.getElementById("pink");
-let mediaIntro = window.matchMedia("(max-width: 415px)");
+
 
 function moveImg () {
     if (mediaResize.matches) {
@@ -17,14 +15,8 @@ function moveImg () {
 moveImg(mediaResize);
 mediaResize.addListener(moveImg);
 
-function moveTextBlock () {
-    if(mediaIntro.matches) {
-        pinkPicture.remove();
-    } 
-}
 
-moveTextBlock (mediaIntro); 
-mediaIntro.addListener(moveTextBlock);
+
 // Animations
 let tl = gsap.timeline();
 
