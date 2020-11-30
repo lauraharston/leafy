e9b9dc7b0cddec6b4ac281620a0f4bb093faed4f
lauraharston <lauraharston@gmail.com>
2020-11-30 15:53:07 -0600

    used gsap to fade in the logo instead of css

--- animation.js
+++ animation.js
@@ -18,7 +18,13 @@ mediaResize.addListener(moveImg);
 
 
 // Animations
-let tl = gsap.timeline();
 
+
+let tl = gsap.timeline();
+tl.from(".logo",{duration: 4, opacity: 0})
+tl.from(".header", {duration: 1, opacity: 0, y: 150})
+tl.from("#feature1", {duration: 0.8, opacity: 0, x: 150})
+tl.from("#feature2", {duration: 0.8, opacity: 0, x: 150})
+tl.from("#feature3", {duration: 0.8, opacity: 0, x: 150})
 tl.from("#hand", {duration: 1, opacity: 0, x: 150, stagger: 0.3})
 tl.from("#herb", {duration: 2, opacity: 0, y: 150, stagger: 0.3})
