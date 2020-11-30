030925aeb042f8e1899d50443fe5006a666ed2c1
lauraharston <lauraharston@gmail.com>
2020-11-30 16:27:53 -0600

    added gsap animations to all the needed sections

--- animation.js
+++ animation.js
@@ -21,10 +21,16 @@ mediaResize.addListener(moveImg);
 
 
 let tl = gsap.timeline();
-tl.from(".logo",{duration: 4, opacity: 0})
+tl.from(".logo",{duration: 2, opacity: 0})
+tl.from("#motto", {duration: 0.5, opacity: 0, x:-150})
 tl.from(".header", {duration: 1, opacity: 0, y: 150})
 tl.from("#feature1", {duration: 0.8, opacity: 0, x: 150})
 tl.from("#feature2", {duration: 0.8, opacity: 0, x: 150})
 tl.from("#feature3", {duration: 0.8, opacity: 0, x: 150})
-tl.from("#hand", {duration: 1, opacity: 0, x: 150, stagger: 0.3})
-tl.from("#herb", {duration: 2, opacity: 0, y: 150, stagger: 0.3})
+tl.from('.animated-col',{duration: 1, opacity: 0, y: 150})
+tl.from('.decor',{duration: 2, opacity: 0, x: -150 })
+tl.from("#hand", {duration: 1, opacity: 0, x: 150})
+tl.from("#herb", {duration: 2, opacity: 0, y: 150})
+tl.from("#vertical-txt",{duration: 1, opacity: 0})
+tl.from("#isaiah", {duration: 1, opacity: 0, y: 150})
+tl.from("#martin", {duration: 1, opacity: 0, x: 150})
