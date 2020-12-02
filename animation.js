49f5063c7a7c697951894be6190329f92ab0ea20
lauraharston <lauraharston@gmail.com>
2020-12-02 09:03:23 -0600

    created different timelines for the decor section

--- animation.js
+++ animation.js
@@ -47,6 +47,50 @@ let tl3 = gsap.timeline(
       }
 );
 
+let tl3a = gsap.timeline(
+  {   
+      scrollTrigger: {
+        trigger: "#decor",
+        scrub: 1, 
+        start: "top center",
+        end: "top 100px"
+      }
+    }
+);
+
+let tl3b = gsap.timeline(
+  {   
+      scrollTrigger: {
+        trigger: "#decor",
+        scrub: 1, 
+        start: "top center",
+        end: "top 100px"
+      }
+    }
+);
+
+let tl3c = gsap.timeline(
+  {   
+      scrollTrigger: {
+        trigger: "#decor",
+        scrub: 1, 
+        start: "top center",
+        end: "top 100px"
+      }
+    }
+);
+
+let tl3d = gsap.timeline(
+  {   
+      scrollTrigger: {
+        trigger: "#decor",
+        scrub: 1, 
+        start: "top center",
+        end: "top 100px"
+      }
+    }
+);
+
 let tl4 = gsap.timeline(
     {   
         scrollTrigger: {
@@ -92,7 +136,10 @@ tl2.from(".header", {duration: 1, opacity: 0, y: 150})
 
 
   tl3.from('.decor',{duration: 1, opacity: 0, x: -150 })
-      .from('.animated-col',{duration: 1, opacity: 0, y: 150})
+  tl3a.from('#imgAnimated2',{duration: 1, opacity: 0, x: -100})
+  tl3b.from('#imgAnimated3',{duration: 1, opacity: 0, x: 100})
+  tl3c.from('#imgAnimated1',{duration: 1, opacity: 0, y: -100})
+  tl3d.from('#imgAnimated4',{duration: 1, opacity: 0, y: 100})
 
 
   tl4.from("#hand", {duration: 5, opacity: 0, x: 100})
